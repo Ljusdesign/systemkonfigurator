13a2f32d4056db014f21903bd228b0a662ddb11a
Simon <pinne@xps.lan>
2020-12-04 13:31:05 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import produce from 'immer'
+import './App.css'
 
+const drivers = {
+  '12v': [
+    {
+      'SNP15w': {
+        name: 'Snappy 15w',
+        power: 15,
+        voltage: 12,
+      }
+    }
+  ],
+  'cc': {
+    'LD30': {
+      name: 'LD30w',
+      current: 0.7,
+      maxPower: 30,
+      minVoltage: 6,
+      maxVoltage: 43,
+    }
+  }
+}
+
+const fixtures = {
+  'GizmoCC': {
+    name: 'Gizmo CC for LED-profile',
+    voltage: 3,
+  }
+}
+
+const System = ({
+  driver,
+  tracks = [],
+  fixtures = [],
+}) => ({
+  driver, tracks, fixtures,
+  toString: () => console.log(driver.name),
+})
+
+const CCSystem = Object.create(System({
+  driver: drivers.cc.LD30,
+  track: 'LED-Profile',
+  fixtures: [],
+}))
+CCSystem.fixtureVoltage = () => this.fixtures.reduce((acc, curr) => acc + curr.voltage, 0)
+const newSystem = produce(CCSystem, draftState => {
+  draftState.fixtures.push([fixtures.GizmoCC, fixtures.GizmoCC])
+})
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Systemkonfigurator</h1>
+      <p>Fixtures in system: {newSystem.fixtureVoltage()}</p>
+      <h2>New state</h2>
+      <pre style={{
+        textAlign: 'left',
+        background: '#ccc',
+        display: 'inline-block',
+        padding: '1em',
+      }}>
+        {JSON.stringify(newSystem, null, 2)}
+      </pre>
     </div>
   );
 }
