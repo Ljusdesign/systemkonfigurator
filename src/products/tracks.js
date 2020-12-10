9d6bbf86a063eebdafd0b588a5daed6c2376adb5
Simon Kers <simon@ljusdesign.se>
2020-12-10 08:02:33 +0100

    Tracks are stored as Map

--- src/products/tracks.js
+++ src/products/tracks.js
@@ -1,28 +1,28 @@
-export default = {
-  "PicoTracLow": {
+export default new Map([
+  ["PicoTracLow", {
     name: "PicoTrac Low",
     height: 10,
     voltage: 12,
     maxAmpere: 5,
-  },
-  "PicoTracHigh": {
+  }],
+  ["PicoTracHigh", {
     name: "PicoTrac High",
     height: 17,
     voltage: 12,
     maxAmpere: 10,
-  },
-  "MiniTrac": {
+  }],
+  ["MiniTrac", {
     name: 'MiniTrac',
     voltage: 12,
     maxAmpere: 20,
-  },
-  "LEDProfileHigh": {
+  }],
+  ["LEDProfileHigh", {
     name: 'LED-Profile High',
     height: 17,
-  },
-  "LEDProfileLow": {
+  }],
+  ["LEDProfileLow", {
     name: 'LED-Profile Low',
     height: 10,
-  },
-}
+  }],
+])
 
