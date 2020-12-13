35f7025ae938da525b1cb568c1581b2e87036180
Simon Kers <simon@ljusdesign.se>
2020-12-13 20:41:45 +0100

    Adding fixture product photos

--- src/products/CCfixtures.js
+++ src/products/CCfixtures.js
@@ -11,18 +11,35 @@ const CCfixtures = new Map([
     shortName: "Arty Profile",
     voltage: 12,
     maxCurrent: 0.5,
+    image: 'arty-profile-zoom-pt.jpg',
+  }],
+  ['Dinkey 450lm', {
+    name: 'Dinkey 450lm CC for LED-profile',
+    shortName: "Dinkey 450lm",
+    voltage: 12,
+    maxCurrent: 0.500,
+    image: 'dinkey-pt.jpg',
+  }],
+  ['Dinkey 250lm', {
+    name: 'Dinkey CC for LED-profile',
+    shortName: "Dinkey 250lm",
+    voltage: 3,
+    maxCurrent: 1.050,
+    image: 'dinkey-pt.jpg',
   }],
   ['Gizmo', {
     name: 'Gizmo CC for LED-profile',
     shortName: "Gizmo",
     voltage: 3,
     maxCurrent: 0.7,
+    image: 'gizmo-pt.jpg',
   }],
   ['Gimmick', {
     name: 'Gizmo CC for LED-profile',
     shortName: "Gimmick",
     voltage: 3,
     maxCurrent: 0.7,
+    image: 'gimmick-pt.jpg',
   }],
 ])
 
