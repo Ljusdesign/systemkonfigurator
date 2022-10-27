f09c0a8a1e8b65d8fe68c6636074cdeafb216d19
Simon Kers <simon@ljusdesign.se>
2022-10-27 21:01:02 +0200

    Dropdown works

--- src/features/system/Settings.js
+++ src/features/system/Settings.js
@@ -5,33 +5,28 @@ import styles from './Settings.module.css'
 
 const makeLabel = current => current + ' mA'
 
-const getOptions = (index, settings, changeSetting) => (
+const getOptions = (settings) => (
   settings.map(op => ({
     value: op.current,
-    isOptionSelected: op.current === op.current,
     label: makeLabel(op.current),
-    onChange: () => changeSetting(index, op.current),
   }))
 )
 
 function Settings({ selectedSettings, outputs, settings, changeSetting }) {
+  const options = getOptions(settings)
   return (
     <div className={styles.outputList}>
       {outputs.map((output, index) => (
         <div key={index}>
           <p>Ch. {index + 1}</p>
           <Select
-            options={getOptions(index, settings, changeSetting)}
-            defaultValue={{
-              value: selectedSettings[index].current,
-              label: makeLabel(selectedSettings[index].current),
-            }}
-            onChange={(event) => changeSetting(index, event)}
+            options={options}
+            defaultValue={options.filter(o => o.value === selectedSettings[index].current)}
+            onChange={(e) => changeSetting(index, e)}
           />
           <Meters index={index} />
         </div>
-      )
-      )}
+      ))}
     </div>
   )
 }
